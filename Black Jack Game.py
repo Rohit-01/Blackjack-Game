5a03956761fe7dd2c7e2ac1361ebbc2b9b1ac218
M Rohit Singha <42908618+Rohit-01@users.noreply.github.com>
2022-02-26 14:14:51 +0530

    Update Black Jack Game.py

--- Black Jack Game.py
+++ Black Jack Game.py
@@ -122,19 +122,19 @@ while game_on:
 
         else:
             print('WAR!')
-            if len(player_one.all_cards) < 5:
+            if len(player_one.all_cards) < 3:
                 print("Player One unable to play war! Game Over at War")
                 print("Player Two Wins! Player One Loses!")
                 game_on = False
                 break
 
-            elif len(player_two.all_cards) < 5:
+            elif len(player_two.all_cards) < 3:
                 print("Player Two unable to play war! Game Over at War")
                 print("Player One Wins! Player One Loses!")
                 game_on = False
                 break
             else:
-                for num in range(5):
+                for num in range(3):
                     player_one_cards.append(player_one.remove_one())
                     player_two_cards.append(player_two.remove_one())
 
